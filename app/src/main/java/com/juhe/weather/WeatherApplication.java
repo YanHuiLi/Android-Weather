cb6869850f92b570e2f0bebdc4aa2061d4cd529f
unknown <>
2016-04-03 00:52:47 +0800

    Initial commit

---
+++ app/src/main/java/com/juhe/weather/WeatherApplication.java
@@ -0,0 +1,17 @@
+package com.juhe.weather;
+
+import android.app.Application;
+
+import com.thinkland.juheapi.common.CommonFun;
+
+/**
+ * Created by Archer on 2015/12/27.
+ */
+public class WeatherApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CommonFun.initialize(getApplicationContext());
+    }
+}
